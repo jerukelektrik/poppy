080009a7aa6ce2c1390fadc31fd559132cedcc7b
jerukelektrik <jerukelektrik@gmail.com>
2026-06-30 07:16:00 +0700

    perf: dequeue unused WordPress Gutenberg and classic theme styles to eliminate render-blocking CSS

--- inc/assets.php
+++ inc/assets.php
@@ -33,3 +33,14 @@ function poppy_enqueue_assets(): void {
 		array( 'strategy' => 'defer', 'in_footer' => true )
 	);
 }
+
+add_action( 'wp_enqueue_scripts', 'poppy_remove_unused_wp_css', 100 );
+/**
+ * Dequeue unused default WordPress block library and classic theme styles to eliminate render-blocking CSS resources.
+ */
+function poppy_remove_unused_wp_css(): void {
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'classic-theme-styles' );
+}
+
