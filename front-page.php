c1b8414ec6293a5a2270f7b249e1143de5a5ca70
jerukelektrik <jerukelektrik@gmail.com>
2026-07-22 21:22:58 +0700

    style: increase hero main heading responsive font sizes by 2px

--- front-page.php
+++ front-page.php
@@ -39,7 +39,7 @@ get_header();
 					</span>
 				</div>
 				
-				<h2 class="text-4xl sm:text-5xl md:text-[56px] lg:text-[60px] xl:text-[68px] font-black leading-[1.15] mb-6 text-poppy-ink">
+				<h2 class="text-[38px] sm:text-[50px] md:text-[58px] lg:text-[62px] xl:text-[70px] font-black leading-[1.15] mb-6 text-poppy-ink">
 					Belajar Lebih Seru, <br />
 					Raih <span class="text-poppy-accent">Sekolah & <br />Kampus Impian</span>
 				</h2>
